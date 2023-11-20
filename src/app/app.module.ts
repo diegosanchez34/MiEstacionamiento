935c6ff5676d0be7a2a56b1360be35afe57dc8e4
Ricardo <ri.raimilla@duocuc.cl>
2023-11-19 22:07:54 -0300

    nada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule } from '@angular/common/Http';
 
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/estacionamientos/estacionamientos.page.ts
+++ src/app/estacionamientos/estacionamientos.page.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/Http';
-
 @Component({
   selector: 'app-estacionamientos',
   templateUrl: './estacionamientos.page.html',
   styleUrls: ['./estacionamientos.page.scss'],
 })
 export class EstacionamientosPage implements OnInit {
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
   }
