acee493087e9ad4761c3063cc17d31aa53477070
diego <diegosanchez1234567@gmail.com>
2023-12-12 02:21:31 -0300

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
   public appPages = [
+    { title: 'Perfil', url: '/perfil', icon: 'person' },
     { title: 'Mapa', url: '/mapa', icon: 'map' },
     { title: 'Arriendos', url: '/arriendos', icon: 'layers' },
     { title: 'Vehículos', url: '/vehiculos', icon: 'car' },
     { title: 'Estacionamientos', url: '/estacionamientos', icon: 'stop' },
     { title: 'Ajustes', url: '/ajustes', icon: 'settings' },
     { title: 'Ayuda', url: '/ayuda', icon: 'help-circle' },
-    { title: 'Contáctanos', url: 'contactanos', icon: 'mail' }
+    { title: 'Contáctanos', url: '/contactanos', icon: 'mail' }
   ];
-  nombre = "Diego";
-  apellido = "Sanchez";
+  
   constructor() {}
 
 }
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { AlertController, NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  clientes:any = [];
+  duenos:any = [];
+
+  clienteURL = 'http://localhost/estacionamiento_getClientes.php';
+  duenoURL= 'http://localhost/estacionamiento_getDuenos.php';
+
+  tipo: string ="password";
+  hide: boolean = false;
+  NotFound: boolean = true;
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  }) 
+
+  constructor(private http: HttpClient, public Alert: AlertController,private router: Router) {
+    this.http.get(this.clienteURL).subscribe((respuesta:any)=>{this.clientes = respuesta});
+    this.http.get(this.duenoURL).subscribe((respuesta:any)=>{this.duenos = respuesta});
+  }
+
+  ngOnInit() {}
+
+  ojito(){
+    this.hide = !this.hide;
+    if (this.hide)
+      this.tipo = 'password';
+    else
+    this.tipo = 'text';
+  }
+
+  async ingresar(){
+
+    this.NotFound = true;
+
+    if(this.form.controls.email.errors?.['required'] || this.form.controls.password.errors?.['required']){
+      this.AlertaVacio();
+    }
+    else if(this.form.controls.email.errors?.['email'] ){
+      this.AlertaEmail();
+    }
+    else {
+      
+      for (let cliente of this.clientes) {
+        if (cliente.correo === this.form.controls.email.value && cliente.clave === this.form.controls.password.value){
+          localStorage.setItem('usuario',JSON.stringify(cliente)); 
+          localStorage.setItem('tipo','cliente'); 
+          this.NotFound = false;
+          this.router.navigate(['/mapa']);
+          break;
+        }            
+      }
+      if(this.NotFound){
+        for (let dueno of this.duenos) {
+          if (dueno.correo === this.form.controls.email.value && dueno.clave === this.form.controls.password.value){
+            localStorage.setItem('usuario',JSON.stringify(dueno)); 
+            localStorage.setItem('tipo','dueno'); 
+            this.NotFound = false;
+            this.router.navigate(['/mapa']);
+            break;
+          }            
+        }
+      }
+      if(this.NotFound)
+        this.AlertaError();
+    }  
+  }
+
+  async AlertaVacio() {
+    const alert = await this.Alert.create({
+      header: 'Campos vacios',
+      message: 'Ingrese todos los datos de usuario',
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
+
+  async AlertaEmail() {
+    const alert = await this.Alert.create({
+      header: 'Correo invalido',
+      message: 'Ingrese una direccion de correo valida',
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
+
+  async AlertaError() {
+    const alert = await this.Alert.create({
+      header: 'Credenciales Incorrectas',
+      message: 'Los datos ingresados son incorrectos',
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
+
+}
